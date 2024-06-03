c31c5131cbaf8681b9ed67b272deb4d48251790f
Yash Patel <yash1451999@gmail.com>
2024-06-03 18:48:16 -0400

    SYNCHRONIZE_REMOTE

--- src/database/db.js
+++ src/database/db.js
@@ -19,7 +19,7 @@ if (process.env.DATABASE == "production") {
   config.username = process.env.DB_USERNAME_REMOTE;
   config.password = process.env.DB_PASSWORD_REMOTE;
   config.database = process.env.DB_NAME_REMOTE;
-  config.synchronize = process.env.SYNCHRONIZE === "true";
+  config.synchronize = process.env.SYNCHRONIZE_REMOTE === "true";
   config.logging = process.env.DB_LOGGING_REMOTE === "true";
   config.ssl = {
     rejectUnauthorized: false,
