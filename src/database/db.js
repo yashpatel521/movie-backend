ecf46c89b0e43cf51117dc6966c49b38c21d2771
Yash Patel <yash1451999@gmail.com>
2024-06-03 18:09:44 -0400

    env

--- src/database/db.js
+++ src/database/db.js
@@ -1,12 +1,31 @@
 require("dotenv").config();
 const { DataSource } = require("typeorm");
 
-const db = new DataSource({
-  type: process.env.DB_TYPE || "sqlite",
-  database: process.env.DB_PATH || "./movies.db",
+let config = {
+  type: process.env.DB_TYPE,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: ["src/database/tables/*.js"],
-  logging: false,
-  synchronize: Boolean(process.env.SYNCHRONIZE),
-});
+  logging: process.env.DB_LOGGING === "true",
+  synchronize: process.env.SYNCHRONIZE === "true",
+};
+
+if (process.env.DATABASE == "production") {
+  config.host = process.env.DB_HOST_REMOTE;
+  config.port = parseInt(process.env.DB_PORT_REMOTE);
+  config.username = process.env.DB_USERNAME_REMOTE;
+  config.password = process.env.DB_PASSWORD_REMOTE;
+  config.database = process.env.DB_NAME_REMOTE;
+  config.synchronize = process.env.SYNCHRONIZE === "true";
+  config.logging = process.env.DB_LOGGING === "true";
+  config.ssl = {
+    rejectUnauthorized: false,
+  };
+}
+
+const db = new DataSource(config);
 
 module.exports = db;
